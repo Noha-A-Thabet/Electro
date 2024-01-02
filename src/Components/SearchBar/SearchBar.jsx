95f842dabb01a1842be68a3bf0291ba4dbe3d19b
Noha-Thabet <nohaathabet@gmail.com>
2024-01-02 13:07:57 +0100

    feat(dropDown menu ): immplement dropdown menu on search bar

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -3,7 +3,7 @@ import Paper from "@mui/material/Paper";
 import MenuList from "@mui/material/MenuList";
 import MenuItem from "@mui/material/MenuItem";
 import ListItemText from "@mui/material/ListItemText";
-import ListItemIcon from "@mui/material/ListItemIcon";
+import TextField from "@mui/material/TextField";
 
 const SearchBar = () => {
   const [input, setInput] = useState("");
@@ -19,29 +19,53 @@ const SearchBar = () => {
     });
     setDisplayMenu(true);
     setElements(result);
-    console.log(elements);
   };
 
   const inputChangeHandler = (e) => {
     const value = e.target.value;
     setInput(value);
-
-    fetchData(value);
+    if (value.length !== 0) {
+      fetchData(value);
+    } else {
+      setElements([]);
+      setDisplayMenu(false);
+    }
   };
+
   return (
     <>
-      <input
-        type="text"
-        name="value"
+      <TextField
+        id="standard-basic"
+        label="Search"
+        variant="standard"
         value={input}
         onChange={inputChangeHandler}
+        sx={{
+          width: "180px",
+        }}
       />
+
       {displayMenu && (
         <Paper sx={{ width: 320 }}>
-          <MenuList dense>
+          <MenuList
+            dense
+            style={{
+              maxHeight: "130px",
+              overflowY: "scroll",
+              position: "absolute",
+              zIndex: "4000",
+              width: "300px",
+              backgroundColor: "white",
+            }}
+          >
             {elements.map((ele) => {
               return (
-                <MenuItem key={ele.id}>
+                <MenuItem
+                  key={ele.id}
+                  onClick={() => {
+                    console.log(ele.id);
+                  }}
+                >
                   <ListItemText>{ele.name}</ListItemText>
                   <img src={ele.image} alt="" style={{ height: "20px" }} />
                 </MenuItem>
